31a571dac10f6b22b0c424daeffa1490ab7c49e3
Pedro Bacchini <pedrobacchini@outlook.com>
2019-06-11 14:53:36 -0300

    Criação do listener para disnonnect, que retransmite a mensagem someone disconnected.
    Criação do listener para chat message responsavel por retransmitir a mensagem para todos os clientes logado.
    
    Emite uma mensagem someone connect quando alguem entra na sala.
    
    Recebe as mensagens no index.html pelo listener em chat message e adiciona as mesmas a tela com a função append.
    
    Emição da mensagem digitado no input para o socket e adiciona a mesma a tela com a função append.

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-    // res.send('<h1>Hello World</h1>');
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        socket.broadcast.emit('chat message', 'someone disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        socket.broadcast.emit('chat message', msg);
+    });
+
     console.log('a user connected');
+
+    socket.broadcast.emit('chat message', 'someone connect');
 });
 
 http.listen(3000, function () {
